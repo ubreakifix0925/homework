4ffb025741c7f6844af24ad3a866431736d269bb
ubreakifix0925 <2995434071@qq.com>
2024-11-22 18:01:35 +0800

    update-24-11-22-18:00

--- 2.cpp
+++ 2.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <ctime>
+//#include <cmath>
 using namespace std;
 using namespace cv;
 
@@ -17,6 +19,8 @@ void ChangeBlurSize(int,void*){
 }
 
 int main(){
+    int start;
+    float time;
     //VideoCapture video("../video/video.mp4");
     namedWindow("time", cv::WINDOW_AUTOSIZE);//视频进度条
     createTrackbar("time","time",&t,video.get(CAP_PROP_FRAME_COUNT),ChangeTime);
@@ -29,6 +33,7 @@ int main(){
         if(frame.empty()){
             break;
         }
+        start=clock();
         cvtColor(frame,gray,COLOR_BGR2GRAY);//转换为灰度图
         //threshold(gray,binary,110,255,THRESH_BINARY);
         //blur(gray,Blur,Size(5,5));//滤波
@@ -37,6 +42,7 @@ int main(){
         //Laplacian(gray,sharp,CV_8U,1);
         Canny(Blur,canny,110,0);//锐化
         HoughCircles(Blur,circles,HOUGH_GRADIENT,2,10,100,100,20,30);//检测半径20-30的圆形
+        time=(float)(clock()-start)/CLOCKS_PER_SEC;
         for(size_t i=0;i<circles.size();i++){
             Point center(cvRound(circles[i][0]),cvRound(circles[i][1]));
             Point Pt1,Pt2,text;
@@ -46,7 +52,7 @@ int main(){
             text=Point(cvRound(circles[i][0])-50,cvRound(circles[i][1])-30);
             rectangle(frame,Pt1,Pt2,Scalar(0,0,255),15);
             //circle(frame,center,3,Scalar(0,0,255),-1,8,0);
-            putText(frame,"aim",text,2,2,Scalar(255,255,255),1);
+            putText(frame,to_string(time),text,2,1,Scalar(255,255,255),2);
             //circle(frame,center,r,Scalar(0,0,255),3,8,0);
         }//标记圆形
         //imshow("video",gray);
--- README.md
+++
@@ -1,1 +0,0 @@
-仅含源码文件，需手动添加build以及video/video.mp4
